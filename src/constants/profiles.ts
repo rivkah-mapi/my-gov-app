c259704fdaa2dce745f2cd4488d18923a5412394
rivkah-mapi <rivkah@mapi.gov.il>
2026-03-15 12:51:59 +0200

    fix profile const

--- src/constants/profiles.ts
+++ src/constants/profiles.ts
@@ -3,7 +3,7 @@ export const PROFILES = {
   PROFILE_2: 'פרופיל_2_לא_נשוי_לא_נכה_מעל_גיל_71_הכנסה_מתחת_ל_2375',
   PROFILE_3: 'פרופיל_3_לא_נשוי_לא_נכה_מעל_גיל_71_הכנסה_מעל_2375_לא_אקדמאי',
   PROFILE_ALL:'סה_כ_קשישים_באזור',
-    PROFILE_AT_RISK: 'סה"כ_קשישים_במצבי_סיכון'
+    PROFILE_AT_RISK: 'סה_כ_קשישים_במצבי_סיכון'
 } as const;
 
 export type ProfileKey = keyof typeof PROFILES;
